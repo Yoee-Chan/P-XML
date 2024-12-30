6fa192e5f2c29285481d4342cc1fc7fcc561b812
Yoee Chan <yoee_chan@163.com>
2024-12-30 21:35:52 +0800

    feat: Insert node to xml

--- p-xml/src/main/java/com/pxml/main/XmlUtils.java
+++ p-xml/src/main/java/com/pxml/main/XmlUtils.java
@@ -1,19 +1,22 @@
 package com.pxml.main;
 
 import org.dom4j.Document;
+import org.dom4j.DocumentException;
 import org.dom4j.Element;
 import org.dom4j.io.SAXReader;
+import org.dom4j.io.XMLWriter;
+
 import java.io.File;
 import java.io.FileInputStream;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.*;
 
 public class XmlUtils {
     public static Map<String, Map<String, Object>> getObjectFromXML(String path) {
         File file = new File(path);
         Map<String, Map<String, Object>> map = new HashMap<>();
-        try (FileInputStream fileInputStream=new FileInputStream(file)){
+        try (FileInputStream fileInputStream = new FileInputStream(file)) {
             SAXReader saxReader = new SAXReader();
             Document read = saxReader.read(fileInputStream);
             Element rootElement = read.getRootElement();
@@ -32,7 +35,7 @@ public class XmlUtils {
         Map<String, Object> subMap = new HashMap<>();
         String name = element.getName();
         for (Element el : elements) {
-            if (!el.getData().toString().trim().isEmpty() && el.getName()!=null) {
+            if (!el.getData().toString().trim().isEmpty() && el.getName() != null) {
                 subMap.put(el.getName(), el.getData().toString().trim().replace("\n", ""));
             }
             getElement(el, map);
@@ -40,5 +43,21 @@ public class XmlUtils {
         name = map.containsKey(name) ? element.getParent().getName() + name : name;
         map.put(name, subMap);
     }
+
+    public void insertElementToRoot(String path, String nodeName, String nodeValue) {
+        SAXReader saxReader = new SAXReader();
+        try {
+            Document document = saxReader.read(new File(path));
+            Element root = document.getRootElement();
+            Element newElement = root.addElement(nodeName);
+            newElement.setText(nodeValue);
+            XMLWriter writer = new XMLWriter(new FileWriter(path));
+            writer.write(document);
+            writer.close();
+        } catch (DocumentException | IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
 }
 
